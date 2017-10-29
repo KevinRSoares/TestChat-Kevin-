b38f0b303b6fc41866e5fd9bcb206d48aedc28ea
Kevin <Kevin@192.168.1.3>
2017-10-29 18:00:40 -0200

    Projeto cliente re-feito;

--- Cliente/src/com/mballem/app/Cliente.java
+++ Cliente/src/com/mballem/app/Cliente.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mbalem.app;
+package com.mballem.app;
 
 /**
  *
- * @author Kevin
+ * @author Administrador
  */
 public class Cliente {
 
